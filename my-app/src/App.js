03d7cc8e93d079acf452be81ecc2a6c789e1e427
YeeunHeo <gjdpdms2005@gmail.com>
2020-09-17 15:09:28 +0900

    complete 14 (AJAX Fetch)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,41 +7,10 @@ class App extends Component{
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
   
   // 컴포넌트 업로드
-  state = {
-  }
+  state = {}
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            id: 1,
-            title: "Matrix",
-            poster: "https://ww.namu.la/s/97ed6d14776f0c6ff206f64ff447cc8b932f6c43eadd755a54a72d1ada738ba41abcab4e7c823efa25f244894fa8ca2ee44ba1b21731a1b90d181d5ad74432acb38478c22e4018986745a33415ac65839c4e93668aed70b546af9e8df77e6f4453157ba7d35165d81eda10c8e42a37ce"
-          },
-          {
-            id: 2,
-            title: "Full Metal Jacket",
-            poster:"https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
-          },
-          {
-            id: 3,
-            title: "Old Boy",
-            poster: "https://upload.wikimedia.org/wikipedia/ko/4/48/Old_Boy.jpg"
-          },
-          {
-            id: 4,
-            title: "Star Wars",
-            poster: "https://lumiere-a.akamaihd.net/v1/images/avco_payoff_1-sht_v7_lg_32e68793.jpeg?region=0%2C0%2C1620%2C2400&width=960" 
-          },
-          {
-            id: 5,
-            title: "Trainspotting",
-            poster: "https://upload.wikimedia.org/wikipedia/en/7/71/Trainspotting_ver2.jpg"
-          }
-        ]
-      })
-    }, 5000)
+    console.log(fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating'))
   }
 
   _renderMovies = () => {
