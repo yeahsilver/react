a08edf4f8043b1710e3b831f9515c77c43a992f0
YeeunHeo <gjdpdms2005@gmail.com>
2020-09-17 14:18:28 +0900

    complete 13 (functional Component)

--- my-app/src/Movie.js
+++ my-app/src/Movie.js
@@ -2,32 +2,29 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-
-class Movie extends Component{
-
-    static propType = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-    render(){
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+function Movie({title, poster}){
+    return (
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component{
-
-    static propTypes = {
-        poster:PropTypes.string.isRequired
-    }
-    render(){
-        return (
-            <img src={this.props.poster}/>
-        )
-    }
+// functional 컴포넌트를 사용할 경우
+function MoviePoster({poster}){
+    return(
+        <img src={poster} alt="Movie Poster"/>
+    )
 }
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
+}
+
 export default Movie
