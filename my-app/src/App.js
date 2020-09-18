9178d3196141f62b8893523480cdf1613ecb5087
YeeunHeo <gjdpdms2005@gmail.com>
2020-09-18 17:08:52 +0900

    complete 17 (Updating Movie)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,7 +16,13 @@ class App extends Component{
   _renderMovies = () => {
     const movies = this.state.movies.map( ( movie ) => {
       // index 대신 movie.id를 사용하면 속도가 향상됨.
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
+      return <Movie 
+      title={movie.title_english} 
+      poster={movie.medium_cover_image} 
+      key={movie.id} 
+      genres={movie.genres}
+      synopsis={movie.synopsis}
+      />
     })
     return movies
   }
--- my-app/src/Movie.js
+++ my-app/src/Movie.js
@@ -2,29 +2,52 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-function Movie({title, poster}){
+function Movie({title, poster, genres, synopsis}){
     return (
-        <div>
-            <MoviePoster poster={poster}/>
-            <h1>{title}</h1>
+        <div className="Movie">
+            <div className="Movie__Columns">
+                <MoviePoster poster={poster} alt={title}/>
+            </div>
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Movie_Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
+                </div>
+                <p className="Movie__Synopsis">
+                    {synopsis}
+                </p>
+            </div>
         </div>
     )
 }
 
 // functional 컴포넌트를 사용할 경우
-function MoviePoster({poster}){
+function MoviePoster({poster, alt}){
     return(
-        <img src={poster} alt="Movie Poster"/>
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster"/>
+    )
+}
+
+function MovieGenre({genre}){
+    return (
+        <span className="Movie__Genre">{genre} </span>
     )
 }
 
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
 }
 
 MoviePoster.propTypes = {
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired,
 }
 
 export default Movie
