b4666d75ffec870d6c61cc66da43c7194955ed53
YeeunHeo <gjdpdms2005@gmail.com>
2020-10-03 22:51:23 +0900

    firebase cloud setup

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
+import { dbService } from "fBase";
 
 const Home = ()=>  {
     const [nweet, setNweet] = useState("");
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        await dbService.collection("nweets").add({
+            nweet, 
+            createdAt: Date.now(),
+        });
+        setNweet("");
     };
     const onChange = (event) => {
         const { target: {value}} = event;
