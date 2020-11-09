884793a5b53c18fc6b17ad71a866a922862cf0f0
YeeunHeo <gjdpdms2005@gmail.com>
2020-11-10 00:52:49 +0900

    update login

--- camtact-practice/src/App.js
+++ camtact-practice/src/App.js
@@ -1,89 +1,138 @@
 import './App.css';
 import img from './grid-logo.png';
-import logo from './logo.png';
-import { Grid, TextField, Button, InputAdornment, Checkbox, Paper } from '@material-ui/core';
-import { AccountCircle, LockRounded} from "@material-ui/icons";
+import { Grid, TextField, Button, Checkbox, Paper, CssBaseline, Avatar, Typography, FormControlLabel, Link , Card} from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import { makeStyles, createMuiTheme } from "@material-ui/core/styles"
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
-function App() {
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: "url("+img+")",
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(5, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(3),
+    backgroundColor: '#253053',
+  },
+  typography:{
+    marginBottom: theme.spacing(3),
+    textColor: '#253053',
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const theme = createMuiTheme({
+  palette:{
+    primary: {
+      main: '#253053',
+    },
+  },
+})
+
+export default function App(){
+  const classes = useStyles();
   return (
-    <div className="App">
-    <Paper>
-      <Grid container style={{minHeight:'100vh'}}>
-      
-        <Grid
-          container
-          item
-          sx={12}
-          sm={6}
-          alignItems="center"
-          direction="column"
-          justify="space-between"
-          style={{padding:10}}
-        >
-          <div />
-          <div
-          style = {{ 
-            display: "flex", 
-            flexDirection:"column", 
-            maxWidth: 400, 
-            minWidth:300
-            }}
-          >
-            <h2 align="left">Log In to Camtact</h2>
-            <TextField label="Username" margin="normal" variant="outlined"/>
-            <TextField label="Password" margin="normal" variant="outlined"/>
+    <ThemeProvider theme={theme}> 
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={12} sm={6} md={5} component={Paper} elevation={6} square>
+
+     
+        <div className={classes.paper}>
+        <Grid container style={{marginLeft:-30, marginTop: 20}}>
+              <Grid item xs>
+                
+              </Grid>
+              <Grid item>
+              <p>Don't have an account? <Link href="#" variant="body2">
+                  {" Sign Up"}
+                </Link>
+              </p>
+                
+              </Grid>
+              
+        </Grid>
+
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography className={classes.typography} component="h1" variant="h5">
+          Sign In
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="id"
+              label="ID"
+              name="id"
+              autoComplete="id"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
 
             <Grid container>
-              <Grid item align="left">
-                
-                <p>
-                <Checkbox
-                  defaultChecked
-                  color="primary"
-                  inputProps={{ 'aria-label': 'secondary checkbox' }}
+              <Grid item xs>
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="자동 로그인"
                 />
-                자동 로그인
-                </p>
               </Grid>
-              {/* <Grid item align="right">
-              <p style={{margin:20, spacing:2}}> 메롱</p>
-              </Grid> */}
-            </Grid>
-
-            <div style={{ height: 20, margin: 30}}>
-              <Button variant="contained" color="primary" >
-                Log In
-              </Button>
-            </div>
-            <div style={{ height: 20, margin:30 }}>
-              <Button variant="outlined">Intereseted in joining?</Button>
-            </div>
-            {/* <Grid container justify="center" spacing={2}>
-              <Grid item>
-                <Button variant="outlined" style={{margin:20}}> Forgot Password? </Button>
+              <Grid item style={{paddingTop:10}}>
+             Miss ID or Password?
+                <Link href="#" variant="body2">
+                  {" Find"}
+                </Link>
               </Grid>
-
-            </Grid> */}
-
-           
-          </div>
-          <div />
-        </Grid>
-
-        <Grid container xs={12} sm={6} alignItems= "center" style={{padding: 10}}>
-          <div />
-          <div>
-            <Grid container justify='center'>
-              <img src={img}style={{width: '100%', height: '100%', objectFit: 'cover'}} alt="camtact"/>
             </Grid>
-          </div>
             
-        </Grid>
-       
+            <Button
+              backgroundColor="primary"
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+          </form>
+        </div>
       </Grid>
-      </Paper>
-    </div>
+      <Grid item xs={false} sm={6} md={7} className={classes.image} />
+    </Grid>
+    </ThemeProvider>
   );
 }
-
-export default App;
